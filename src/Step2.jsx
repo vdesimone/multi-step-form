7c84d7f2f43725eda06878c0a8171a4a2e2b6fa4
vdesimone <173499012+vdesimone@users.noreply.github.com>
2024-12-18 09:25:00 -0500

    Add `<form>` tag to `Step1`, and create, style, and structure `Step2`

---
+++ src/Step2.jsx
@@ -0,0 +1,75 @@
+import arcadeIcon from "./assets/icon-arcade.svg";
+import advancedIcon from "./assets/icon-advanced.svg";
+import proIcon from "./assets/icon-pro.svg";
+
+function Step2() {
+
+  return(
+    <div className="step-container">
+      <form>
+        <h2>Select your plan</h2>
+        <p>You have the option of monthly or yearly billing.</p>
+
+        <div className="plan-container">
+          <div className="plan">
+            <input
+              type="radio"
+              name="plan"
+              id="arcade"
+              onChange=""
+              checked
+            />
+            <label htmlFor="arcade">
+              <img src={arcadeIcon} alt="Arcade Plan Icon" />
+              <h4>Arcade</h4>
+              <p>$9/mo</p>
+            </label>
+          </div>
+
+          <div className="plan">
+            <input
+              type="radio"
+              name="plan"
+              id="advanced"
+              onChange=""
+            />
+            <label htmlFor="advanced">
+              <img src={advancedIcon} alt="Advanced Plan Icon" />
+              <h4>Advanced</h4>
+              <p>$12/mo</p>
+            </label>
+          </div>
+
+          <div className="plan">
+            <input
+              type="radio"
+              name="plan"
+              id="pro"
+              onChange=""
+            />
+            <label htmlFor="pro">
+              <img src={proIcon} alt="Pro Plan Icon" />
+              <h4>Pro</h4>
+              <p>$15/mo</p>
+            </label>
+          </div>
+        </div>
+
+        <div className="toggle-container">
+          <div className="toggle-content">
+            <h5 className="selected">Monthly</h5>
+            <input type="checkbox" id="toggle-switch" />
+            <label htmlFor="toggle-switch"></label>
+            <h5>Yearly</h5>
+          </div>
+        </div>
+      </form>
+      <div className="buttons">
+          <button className="go-back">Go Back</button>
+          <button className="next">Next Step</button>
+        </div>
+    </div>
+  );
+}
+
+export default Step2;
